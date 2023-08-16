b35a5b6df02cd0f020ca1699b20f86eac83dfc42
Hernan Castro <magehernan@gmail.com>
2023-08-16 18:56:49 -0300

    fix test that was using + to add.
    Added test that throw with invalid send amount to setBid

--- test/Veritrust.js
+++ test/Veritrust.js
@@ -6,11 +6,10 @@ describe("Veritrust Smart Contract", function () {
   let veritrustFactory;
   let owner;
   let bidder;
-  let veritrustContractAddress;
 
-  const bidFee = ethers.utils.parseEther("0.002");
-  const deployFee = ethers.utils.parseEther("0.001");
-  const warrantyAmount = ethers.utils.parseEther("0.003");
+  const bidFee = ethers.utils.parseEther("0.1");
+  const deployFee = ethers.utils.parseEther("0.5");
+  const warrantyAmount = ethers.utils.parseEther("1");
 
   const deployVeritrust = async () => {
     const tx = await veritrustFactory.connect(licitante).deployVeritrust(
@@ -22,11 +21,12 @@ describe("Veritrust Smart Contract", function () {
     { value: deployFee })
     
     const receipt = await tx.wait()
-    console.log(receipt.events.forEach(event => console.log(event)))
-
-    veritrustContractAddress = receipt.events.filter(event => event.event == "ContractDeployed")[0].args.contractAddress;
-
-    return await ethers.getContractAt("Veritrust", veritrustContractAddress);
+   
+    const veritrustContractAddress = receipt.events.filter(event => event.event == "ContractDeployed")[0].args.contractAddress;
+    const Veritrust = await ethers.getContractFactory("Veritrust");
+    const veritrust = Veritrust.attach(veritrustContractAddress);
+    await veritrust.deployed();
+    return veritrust;
 
   }
 
@@ -40,33 +40,41 @@ describe("Veritrust Smart Contract", function () {
       deployFee,
       bidFee
     );
+    
     await veritrustFactory.deployed();
-
   });
 
   it.skip("Should deploy veritrust contract", async function () {
-    
-    let veritrust = await deployVeritrust();
-    
+    await deployVeritrust();    
   }); 
 
   
   it("should set a bid", async function () {
     const bidderName = "Bidder1";
     const urlHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
-
-    let veritrust = await deployVeritrust();
-
+    const veritrust = await deployVeritrust();
+   
     const initialBalance = await ethers.provider.getBalance(veritrust.address);
-    console.log(await ethers.provider.getBalance(bidder.address));
-
-    // console.log({ bidFee }, { warrantyAmount })
-
-    expect(await veritrust.connect(bidder).setBid(bidderName, urlHash, { value: bidFee + warrantyAmount }));
+   
+    await veritrust.connect(bidder).setBid(bidderName, urlHash, { value: bidFee.add(warrantyAmount) });
 
     const newBalance = await ethers.provider.getBalance(veritrust.address);
     
-    expect(newBalance).to.equal(initialBalance.add(warrantyFee));
+    expect(newBalance).to.equal(initialBalance.add(warrantyAmount));
+  });
+ 
+  it("should throw at set a bid", async function () {
+    const bidderName = "Bidder1";
+    const urlHash = "0x1234567890123456789012345678901234567890123456789012345678901234";
+    const veritrust = await deployVeritrust();
+   
+    const initialBalance = await ethers.provider.getBalance(veritrust.address);
+   
+    await expect(veritrust.connect(bidder).setBid(bidderName, urlHash, { value: bidFee })).to.be.revertedWith("Incorrect payment fee");
+
+    const newBalance = await ethers.provider.getBalance(veritrust.address);
+    
+    expect(newBalance).to.equal(initialBalance);
   });
   
 });
