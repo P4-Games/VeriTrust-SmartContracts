b670b813252aa7fce815dc72ff323f54024c2de1
Hernan Castro <magehernan@gmail.com>
2023-09-07 14:36:30 -0300

    Change config and format of forge

--- src/OracleMock.sol
+++ src/OracleMock.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: UNLICENCED
 pragma solidity 0.8.19;
 
-import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract OracleMock is AggregatorV3Interface {
-  function decimals() external view returns (uint8) {
-    return 8;
-  }
+    function decimals() external pure returns (uint8) {
+        return 8;
+    }
 
-  function description() external view returns (string memory) {
-    return '';
-  }
+    function description() external pure returns (string memory) {
+        return "";
+    }
 
-  function version() external view returns (uint256) {
-    return 1;
-  }
+    function version() external pure returns (uint256) {
+        return 1;
+    }
 
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-  {}
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    { }
 
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-  {
-    answer = 177_800_000_000;
-  }
+    function latestRoundData() external pure returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+        answer = 177_800_000_000;
+        roundId = 0;
+        startedAt = 0;
+        updatedAt = 0;
+        answeredInRound = 0;
+    }
 }
--- test/VeritrustFactoryTest.sol
+++ test/VeritrustFactoryTest.sol
@@ -1,24 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from 'forge-std/Test.sol';
-import '../src/VeritrustFactory.sol';
-import '../src/OracleMock.sol';
+import { Test, console2 } from "forge-std/Test.sol";
+import "../src/VeritrustFactory.sol";
+import { OracleMock } from "../src/OracleMock.sol";
 
 contract VeritrustFactoryTest is Test {
-  address owner = makeAddr('owner');
-  address alice = makeAddr('alice');
-  address bob = makeAddr('bob');
+    address owner = makeAddr("owner");
+    address alice = makeAddr("alice");
+    address bob = makeAddr("bob");
 
-  VeritrustFactory factory;
-  OracleMock oracle;
+    VeritrustFactory factory;
+    OracleMock oracle;
+    uint256 deployFee = 1 ether;
+    uint256 bidFee = 1 ether;
 
-  function setUp() public {
-    oracle = new OracleMock();
-    factory = new VeritrustFactory(1, 1, address(oracle));
-  }
+    function setUp() public {
+        oracle = new OracleMock();
+        factory = new VeritrustFactory(deployFee, bidFee, address(oracle));
+    }
 
-  function test_Increment() public {}
+    function test_Create_Veristrust() public {
+        string memory name = "Test";
+        string memory ipfsUrl = "www.test.com.ar";
+        uint128 commitDeadline = 172_800;
+        uint128 revealDeadline = 172_800;
+        uint256 warrantyAmount = 1 ether;
 
-  function testFuzz_SetNumber(uint256 x) public {}
+        factory.deployVeritrust{ value: 1 ether }(name, ipfsUrl, commitDeadline, revealDeadline, warrantyAmount);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public { }
 }
